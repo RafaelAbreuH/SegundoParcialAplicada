70b9dbc65d0aa67334a9c0503108f5f2e0e21b4e
Rafael Abreu Hernandez <Rafael_abreu5@ucne.edu.do>
2018-07-02 18:02:36 -0400

    ...

--- SegundoParcial/DAL/Script/Script.sql
+++ SegundoParcial/DAL/Script/Script.sql
@@ -2,13 +2,3 @@ create database Parcial2Db
 go
 use Parcial2Db
 go
-/*create table Articulos
-(
-
-	ArticuloId int primary key identity(1,1),
-	Descripcion varchar,
-	Precio int,
-	CantidadCotizada int,
-	FechaVencimiento DateTime
-
-);*/
